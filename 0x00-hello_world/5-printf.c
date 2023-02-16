5491b0cb49aa3b5a290684f49f3124e0edcd247d
NOCKIS <kibetenock97@gmail.com>
2023-02-16 07:49:39 -0800

    C is fun

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h> 
+/**
+ * * main: programs that prints using printf
+ * * return:0 if program sucees
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
+
