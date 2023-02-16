f7995cc80f73bd948fe61f4ae75d4c3a0d15bc57
NOCKIS <kibetenock97@gmail.com>
2023-02-16 10:20:47 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
-#include <stdio.h> 
+#!/bin/bash
+#include <stdio.h>
 /**
- * * main: programs that prints using printf
- * * return:0 if program sucees
- */
+*main - Entry point
+*Return: Always 0 (Success)
+*/
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
-
