122f46985cbab9c82eaac0ead171a7d598589f1c
NOCKIS <kibetenock97@gmail.com>
2023-02-16 07:02:31 -0800

    4-puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - program that prints programming is like a building 
+ * Return:0(programcompleted)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
